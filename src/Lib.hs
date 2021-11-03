f8e4f555af7c0979a029d101563886464009ee9d
YaroslavKozhevnikov-codeit <yaroslav.kozhevnikov@codeit.pro>
2021-11-03 04:31:45 +0200

    draft: linear flow for reactive services

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,8 @@
 {-# LANGUAGE AllowAmbiguousTypes   #-}
 {-# LANGUAGE DataKinds             #-}
+{-# LANGUAGE DeriveAnyClass        #-}
 {-# LANGUAGE DeriveFunctor         #-}
+{-# LANGUAGE DeriveGeneric         #-}
 {-# LANGUAGE FlexibleInstances     #-}
 {-# LANGUAGE GADTs                 #-}
 {-# LANGUAGE InstanceSigs          #-}
@@ -10,21 +12,19 @@
 {-# LANGUAGE TypeApplications      #-}
 {-# LANGUAGE TypeFamilies          #-}
 {-# LANGUAGE TypeSynonymInstances  #-}
-{-# LANGUAGE DeriveAnyClass        #-}
-{-# LANGUAGE DeriveGeneric         #-}
 
 module Lib
     ( someFunc
     ) where
 
-import qualified Control.Monad.State as S
-import           Data.Kind           (Type)
-import           GHC.Generics
-import Data.Void (Void)
-import Control.Monad (void)
+import           Control.Concurrent     (MVar, forkIO, newEmptyMVar, putMVar,
+                                         takeMVar, threadDelay)
+import           Control.Concurrent.STM
+import           Control.Monad.Reader
+import           Control.Monad.STM
+import           Data.Kind              (Type)
 
 newtype Key = Key { unKey :: Int }
-
 
 data Created
 data InProcess
@@ -33,101 +33,71 @@ data Processed
 data First
 data Second
 
-class SMachine t m where
-  data SState (t :: EntityType) m s :: Type
+data EntityState s where
+  Created :: { createdKey :: Key } -> EntityState Created
+  InProcess :: { inProcessKey :: Key } -> EntityState InProcess
+  Processed :: { processedKey :: Key } -> EntityState Processed
 
-  getCreatedEntity :: Key -> m (SState t m Created)
+class LinearState m where
+  type State m :: Type -> Type
 
-  createEntity :: Key -> m (SState t m Created)
-  processEntity :: SState t m Created -> m (SState t m Processed)
+  createEntity :: Key -> m (State m Created)
+  getCreatedEntity :: Key -> m (State m Created)
+  awaitCreatedEntity :: TQueue Key -> m (State m Created)
 
-data EntityType = First | Second
+  processEntity :: State m Created -> m (State m InProcess)
+  getInProcessEntity :: Key -> m (State m InProcess)
+  awaitInProcessEntity :: TQueue Key -> m (State m InProcess)
 
-type FakeState = S.State EntityType
+  completeEntity :: State m InProcess -> m (State m Processed)
 
-instance SMachine 'First FakeState where
-  data SState 'First FakeState s where
-    Created1 :: { created1Key :: Key } -> SState 'First FakeState Created
-    Processed1 :: { processed1Key :: Key } -> SState 'First FakeState Processed
+instance LinearState IO where
+  type State IO = EntityState
+  createEntity k = return $ Created k
+  getCreatedEntity k = return $ Created k
+  awaitCreatedEntity tq = atomically $ do
+    k <- readTQueue tq
+    return $ Created k
 
-  getCreatedEntity k = do
-    return $ Created1 k
-
-  createEntity k = do
-    S.put First
-    return $ Created1 k
-  processEntity Created1 {..} = do
-    S.put First
-    return $ Processed1 created1Key
-
-instance SMachine 'Second FakeState where
-  data SState 'Second FakeState s where
-    Created2 :: { created2Key :: Key } -> SState 'Second FakeState Created
-    Processed2 :: { processed2Key :: Key } -> SState 'Second FakeState Processed
-
-  getCreatedEntity k = do
-    return $ Created2 k
-
-  createEntity k = do
-    S.put Second
-    return $ Created2 k
-  processEntity Created2 {..} = do
-    S.put Second
-    return $ Processed2 created2Key
-
-data EntityState (t :: EntityType) s where
-  Created :: { createdKey :: Key } -> EntityState t Created
-  InProcess :: { inProcessKey :: Key } -> EntityState t InProcess
-  Processed :: { processedKey :: Key } -> EntityState t Processed
-
-class MetaMachine m where
-  type State (t :: EntityType) m :: Type -> Type
-
-  getInProcessEntity :: Key -> m (State t m InProcess)
-
-  createNewEntity :: Key -> m (State t m Created)
-  delegateEntity :: (SMachine t m) => State t m Created -> (Key -> m (SState t m Created)) -> m (State t m InProcess)
-  delegateEntity2 :: (SMachine t m) => State t m InProcess -> (SState t m Created -> m (SState t m Processed)) -> m (State t m Processed)
-
-instance MetaMachine FakeState where
-  type State t FakeState = EntityState t
-
+  processEntity Created {..} = return $ InProcess createdKey
   getInProcessEntity k = return $ InProcess k
+  awaitInProcessEntity tq = atomically $ do
+    k <- readTQueue tq
+    return $ InProcess k
 
-  createNewEntity key =
-    return $ Created key
-  delegateEntity Created {..} f = do
-    -- let's imagine that we saved it somewhere
-    delegatedEntity <- f createdKey
-    return $ InProcess createdKey
-  delegateEntity2 InProcess {..} f = do
-    -- let's imagine we retrieved stored entity
-    delegatedEntity <- getCreatedEntity inProcessKey
-    f delegatedEntity
-    return $ Processed inProcessKey
+  completeEntity InProcess {..} = return $ Processed inProcessKey
 
-example1 :: FakeState ()
-example1 = do
-  let k = Key 1
-  en <- createNewEntity k
-  en' <- delegateEntity en $ createEntity @'First
-  delegateEntity2 en' processEntity
-  return ()
+eventSender :: TQueue Key -> Key -> IO ()
+eventSender tq k = do
+  putStrLn "> Sending first event"
+  atomically $ writeTQueue tq k
+  putStrLn "> Waiting"
+  threadDelay 1000
+  putStrLn "> Sending second event"
+  atomically $ writeTQueue tq k
+  putStrLn "> Waiting"
+  threadDelay 1000
+  putStrLn "> Sending third event"
+  atomically $ writeTQueue tq k
 
-example2 :: FakeState ()
-example2 = do
-  let k = Key 1
-  en <- createNewEntity k
-  void . delegateEntity en $ createEntity @'First
+worker :: TQueue Key -> MVar () -> IO ()
+worker tq v = do
+  putStrLn ">> Awaiting created entity"
+  cEn <- awaitCreatedEntity tq
+  putStrLn ">> Processing created entity"
+  void $ processEntity cEn
+  putStrLn ">> Awaiting in-process entity"
+  iEn <- awaitInProcessEntity tq
+  putStrLn ">> Finishing entity"
+  void $ completeEntity iEn
+  putMVar v ()
 
 someFunc :: IO ()
-someFunc = return . const () . flip S.runState First $ do
-  example2
-  t <- S.get
+someFunc = do
+  putStrLn "Setting env up"
+  tq <- newTQueueIO
+  mv <- newEmptyMVar
+  tId <- forkIO $ worker tq mv
+  tId2 <- forkIO $ eventSender tq (Key 0)
+  takeMVar mv
   return ()
-  -- en <- case t of
-  --   First -> do
-  --     getInProcessEntity @_ @'First $ Key 1
-  --   Second -> do
-  --     getInProcessEntity @_ @'Second $ Key 1
-  -- delegateEntity2 en processEntity
